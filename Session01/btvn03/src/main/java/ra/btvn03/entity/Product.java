2a77e811869c9a900fb62dc60ebf3d9371f23da4
Vũ Hoàng <153354779+hoangvu201@users.noreply.github.com>
2025-09-08 17:43:39 +0700

    Add files via upload

---
+++ Session01/btvn03/src/main/java/ra/btvn03/entity/Product.java
@@ -0,0 +1,16 @@
+package ra.btvn03.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+}
---
+++ Session01/btvn04/src/test/java/ra/btvn04/Btvn04ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.btvn04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Btvn04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

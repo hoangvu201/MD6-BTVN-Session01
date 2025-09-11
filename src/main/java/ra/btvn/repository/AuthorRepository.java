7f186c2728f5d4a73c57228854a8fb44547fd86a
Vũ Hoàng <153354779+hoangvu201@users.noreply.github.com>
2025-09-12 02:22:03 +0700

    Delete src directory

--- src/main/java/ra/btvn/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package ra.btvn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ra.btvn.model.entity.Author;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author,Long> {
-}
